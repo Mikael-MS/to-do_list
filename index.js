8b928b9274aa67797512f5fef1ccc503960a1184
Mikael Moura <mikael.moura79@gmail.com>
2023-05-05 16:16:13 -0300

    tweak js code

--- index.js
+++ index.js
@@ -1,103 +1,81 @@
 const input = document.getElementById('input');
-console.log(input)
 const btn = document.getElementById('button');
-console.log(btn)
 const list = document.getElementById('list');
-console.log(list)
-const card = document.querySelector('.card');
-console.log(card)
+const card = document.getElementById('card');
 
 
-
-// converte o local storange JSON para array
-const taskis = JSON.parse(localStorage.getItem('taskis')) || [];
-
-function renderTaskis() {
-
-    list.innerHTML = '';
-
-    for(let taski of taskis){
-        //criar uma li para ser adicionada na lista 
-        const itemLi = document.createElement('li');
-
-        // cria evento de click na li 
-        itemLi.addEventListener('click', function(){
-            //remove item da lista através da função 
-            removeItemTask(itemLi)
-        })
-
-        // atribuir uma classe a ela do bootstrap
-        itemLi.setAttribute('class', 'list-group-item list-group-item-action');
-        //adicionar na li um texto que é o valor do input
-        itemLi.textContent = taski;
-        //adicionar a li na lista 
-        list.appendChild(itemLi);
-    }
-}
-
-renderTaskis();
-
-// Evento click no botão
-btn.addEventListener('click', function () {
-    // ao clicar no botão vamos pegar o valor do input
-    let inputValue = input.value;
-
-    // verifica se é o valor do input é vazio
-    if (!inputValue) {
-
-        //limpa msg de erro
-        removeErro();
-        
-        // cria mensagem de erro
-        const spanError = document.createElement('span');
-        spanError.setAttribute('class', 'alert alert-warning');
-        let textError = document.createTextNode('Você precisa adicionar uma tarefa');
-        spanError.appendChild(textError);
-        card.appendChild(spanError);
-
-        
-    } else {
-        
-        //adiconar o item do input a uma array
-        taskis.push(inputValue);
-
-        // imprimir array
-        console.log(taskis);
-
-        //esvazia o input
-        input.value = "";
-
-        //renderiza a tela e insere um valor na lista
-        renderTaskis();
-
-        //limpa msg de erro
-        removeErro();
-
-        // slava na local storange
-        saveDataStorange();
-    
-    }
-})
-
-
-// função remove erro
-function removeErro() {
+function removeError() {
     let spanError = document.querySelectorAll('span');
     for(const span of spanError){
         card.removeChild(span);
     }
 }
 
-//função remove item da array de taskis
+
 function removeItemTask(item){
-    taskis.splice(taskis.indexOf(item.textContent),1);
+    tasks.splice(tasks.indexOf(item.textContent),1);
 
-    renderTaskis();
+    renderTasks(tasks);
 
-    saveDataStorange();
+    saveDataStorage(tasks);
 }
 
-// função que cria um local storange
-function saveDataStorange() {
-    localStorage.setItem('taskis', JSON.stringify(taskis));
+
+function saveDataStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+
+function renderTasks(tasks) {
+    
+    list.innerHTML = '';
+
+    for(let task of tasks){
+    
+        const itemLi = document.createElement('li');
+         
+        itemLi.addEventListener('click', function(){
+             
+            removeItemTask(itemLi)
+        })
+
+      
+        itemLi.setAttribute('class', 'list-group-item list-group-item-action');
+ 
+        itemLi.textContent = task;
+       
+        list.appendChild(itemLi);
+    }
+}
+
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+renderTasks(tasks);
+
+btn.addEventListener('click', function () {
+    
+    let inputValue = input.value;
+
+    if (!inputValue) {
+
+        removeError();
+        
+        const spanError = document.createElement('span');
+        spanError.setAttribute('class', 'alert alert-warning');
+        let textError = document.createTextNode('Você precisa adicionar uma tarefa');
+        spanError.appendChild(textError);
+        card.appendChild(spanError);
+
+    } else {
+        
+        tasks.push(inputValue);
+
+        input.value = "";
+
+        renderTasks();
+
+        removeError();
+
+        saveDataStorage(tasks);
+    }
+})
